51f60b3575c721c8c7b5cdd82e56f971309100d4
Tolga <tolgacnunal@gmail.com>
2025-06-10 23:59:03 +0200

    Better thresholds for configurations and show/hide based on the file type

--- src/main/kotlin/com/github/tolgacanunal/intellijgltfviewerplugin/GltfViewerToolWindowFactory.kt
+++ src/main/kotlin/com/github/tolgacanunal/intellijgltfviewerplugin/GltfViewerToolWindowFactory.kt
@@ -1,10 +1,12 @@
 package com.github.tolgacanunal.intellijgltfviewerplugin
 
-import com.github.tolgacanunal.intellijgltfviewerplugin.GltfHttpServer
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.wm.IdeFocusManager
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
@@ -12,9 +14,14 @@ import com.intellij.ui.jcef.JBCefBrowser
 
 class GltfViewerToolWindowFactory : ToolWindowFactory {
 
+    private var toolWindowRef: ToolWindow? = null
+    private var projectRef: Project? = null
+
     private fun getBaseUrl() = "http://localhost:${GltfHttpServer.port}"
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        this.toolWindowRef = toolWindow
+        this.projectRef = project
         GltfHttpServer.start()
 
         val browser = JBCefBrowser("${getBaseUrl()}/viewer/index.html")
@@ -29,9 +36,15 @@ class GltfViewerToolWindowFactory : ToolWindowFactory {
             FileEditorManagerListener.FILE_EDITOR_MANAGER,
             object : FileEditorManagerListener {
                 override fun selectionChanged(event: FileEditorManagerEvent) {
-                    event.newFile
-                        ?.takeIf { it.extension?.lowercase() in listOf("gltf", "glb") }
-                        ?.also { loadModel(browser, it) }
+                    if (event.newFile?.extension?.lowercase() !in listOf("gltf", "glb")) {
+                        hideToolWindow()
+                        return
+                    }
+                    if (event.newFile == null) {
+                        return
+                    }
+                    loadModel(browser, event.newFile!!)
+                    activateToolWindow()
                 }
             }
         )
@@ -41,6 +54,25 @@ class GltfViewerToolWindowFactory : ToolWindowFactory {
         val path = file.path
         val url = "${getBaseUrl()}/files$path"
         browser.cefBrowser.executeJavaScript("loadGltf('$url')", file.url, 0)
-        browser.openDevtools()
+    }
+
+    private fun activateToolWindow() {
+        if (projectRef == null || toolWindowRef == null || toolWindowRef?.isVisible == true) {
+            return
+        }
+        val editor =  FileEditorManager.getInstance(projectRef!!).selectedEditor
+        val restoreFocus = Runnable {
+            IdeFocusManager.getInstance(projectRef).doWhenFocusSettlesDown {
+                ApplicationManager.getApplication().invokeLater { editor?.component?.requestFocus() }
+            }
+        }
+        toolWindowRef?.activate(restoreFocus, false, false)
+    }
+
+    private fun hideToolWindow() {
+        if (toolWindowRef == null || toolWindowRef?.isVisible == false) {
+            return
+        }
+        toolWindowRef?.hide()
     }
 }
